8b511f2f198f9f178db50fe5a332d5cf465753a7
Bjarne Helland <bjarne81@gmail.com>
2019-10-25 14:25:27 +0200

    Add storybook

---
+++ src/Button/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+type Props = {
+  onClick: (e: React.MouseEvent) => void
+}
+
+const Button: React.SFC<Props> = props => {
+  return <button onClick={props.onClick}>{props.children}</button>
+}
+
+export default Button
