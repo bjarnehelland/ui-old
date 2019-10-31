a96692a96498acf02e09d0efb88150a47de2f9d8
Bjarne Helland <bjarne81@gmail.com>
2019-10-31 06:43:30 +0100

    updated table

--- src/components/Table.stories.tsx
+++ src/components/Table.stories.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react'
-import { text } from '@storybook/addon-knobs'
 import { Table } from './Table'
 
 export default {
   title: 'Table',
 }
 
-const rows = [
-  { team: 'team1', w: 1, d: 3, l: 23324444 },
-  { team: 'team2', w: 1, d: 3, l: 1 },
-  { team: 'team33232 3224', w: 333232, d: 0, l: 2 },
-  { team: 'team4', w: 1, d: 3, l: 2 },
+const data = [
+  { team: 'team1', w: 1000, d: 3, l: 23324444 },
+  { team: 'team2', w: 2000, d: 3, l: 1 },
+  { team: 'team33232 f', w: 333232, d: 0, l: 2 },
+  { team: 'team4', w: 10000, d: 3, l: 2 },
 ]
 
-export const simpleVersion = () => <Table rows={rows} />
+export const simpleVersion = () => <Table data={data} />
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import cx from 'classnames'
+import * as d3 from 'd3-array'
 
 type Row = { [key: string]: any }
 type Column = {
-  title: string
-  link: string
+  header: string
+  key: string
   type: string
+  format: (type: string) => string
 }
 
 export type TableProps = {
-  rows: Row[]
+  data: Row[]
 }
 
 type TableRowProps = {
@@ -22,32 +24,50 @@ const TableRow: React.FC<TableRowProps> = ({ columns, row }) => {
   return (
     <tr>
       {columns.map(column => (
-        <td key={column.link} className={cx([column.type])}>
-          {row[column.link]}
+        <td key={column.key} className={cx([column.type])}>
+          {column.format(row[column.key])}
         </td>
       ))}
     </tr>
   )
 }
 
-export const Table: React.FC<TableProps> = ({ rows }) => {
-  const columns = Object.keys(rows[0]).map(colName => ({
-    title: colName,
-    link: colName,
-    type: typeof rows[0][colName],
-  }))
+function getFormatter(type: string) {
+  if (type === 'number') {
+    var numberFormat = new Intl.NumberFormat('nb-NO')
+    return (value: number) => numberFormat.format(value)
+  }
+
+  return (value: any) => value
+}
+
+export const Table: React.FC<TableProps> = ({ data }) => {
+  const columns = Object.keys(data[0]).map(colName => {
+    const type = typeof data[0][colName]
+    const [min, max] = d3.extent(data, (r: Row) => r[colName])
+    const format = getFormatter(type)
+
+    return {
+      header: colName,
+      key: colName,
+      format,
+      type,
+      min,
+      max,
+    }
+  })
   console.log(columns)
   return (
     <StyledTable>
       <thead>
         <tr>
           {columns.map(column => (
-            <th key={column.link}>{column.title}</th>
+            <th key={column.key}>{column.header}</th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {rows.map((row, i) => (
+        {data.map((row, i) => (
           <TableRow key={i} columns={columns} row={row}></TableRow>
         ))}
       </tbody>
@@ -58,5 +78,10 @@ export const Table: React.FC<TableProps> = ({ rows }) => {
 const StyledTable = styled.table`
   td.number {
     text-align: right;
+  }
+
+  tr {
+    border-bottom: 1px solid #ccc;
+    padding: 10px 0;
   }
 `
