aabd9342f2e76512db0720e53c981f249def6f75
Bjarne Helland <bjarne81@gmail.com>
2019-11-01 18:25:25 +0100

    updated card

--- .storybook/config.js
+++
@@ -1,6 +0,0 @@
-import { configure, addDecorator } from '@storybook/react'
-import { withKnobs } from '@storybook/addon-knobs'
-
-addDecorator(withKnobs)
-// automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.tsx?$/), module)
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,32 +2,17 @@ import * as React from 'react'
 import styled from '@emotion/styled'
 
 export interface CardProps {
-  kind:
-    | 'default'
-    | 'bordered'
-    | 'well'
-    | 'white'
-    | 'raised'
-    | 'floating'
-    | 'popping'
+  kind: 'bordered' | 'raised' | 'floating' | 'popping'
 }
 
 const kinds = {
-  default: ``,
   bordered: `
     box-shadow: 0 0 0 2px rgba(48, 45, 61, 0.1);
-  `,
-  well: `
-    background-color: rgba(26, 14, 35, 0.03);
-    transition-duration: 0.3s;
-  `,
-  white: `
-    background-color: #fff;
-    transition-duration: 0.3s;
   `,
   raised: `
     border: 2px solid rgba(139, 133, 160, 0.1);
     border-bottom: 4px solid rgba(139, 133, 160, 0.25);
+    border-radius: 0.5em;
     background-color: #fff;
   `,
   floating: `
@@ -41,17 +26,18 @@ const kinds = {
     transition-duration: 0.4s;
   `,
 }
-const getKind = ({ kind = 'default' }: CardProps) => kinds[kind]
+const getKind = ({ kind = 'bordered' }: CardProps) => kinds[kind]
 
 const StyledCard = styled('div')<CardProps>`
   box-sizing: border-box;
   background-color: transparent;
-  border-radius: 0.5em;
+  border-radius: 3px;
   box-shadow: none;
   color: #3f2454;
   display: block;
   padding: 1.25rem 1.375rem;
   transition: all 0.25s ease-out;
+  margin: 10px;
   ${getKind};
 `
 
